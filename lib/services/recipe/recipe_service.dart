ee834c037017ec6df372416798fb0e8ee7aa41ad
wassimhaoues <wessimhws@gmail.com>
2026-03-01 03:57:50 +0100

    feat(recipes-service): define user-scoped recipe service interface

--- lib/services/recipe/recipe_service.dart
+++ lib/services/recipe/recipe_service.dart
@@ -1,1 +1,33 @@
-// Recipes module scaffolding. Implementation is added incrementally.
+import '../../models/recipe.dart';
+import '../../models/recipe_category.dart';
+
+abstract interface class RecipeService {
+  Stream<List<Recipe>> watchRecipes({
+    required String uid,
+    RecipeCategory? category,
+    bool favoritesOnly = false,
+  });
+
+  Future<List<Recipe>> fetchRecipes({
+    required String uid,
+    RecipeCategory? category,
+    bool favoritesOnly = false,
+  });
+
+  Future<Recipe?> fetchRecipeById({
+    required String uid,
+    required String recipeId,
+  });
+
+  Future<String> createRecipe({required String uid, required Recipe recipe});
+
+  Future<void> updateRecipe({required String uid, required Recipe recipe});
+
+  Future<void> deleteRecipe({required String uid, required String recipeId});
+
+  Future<void> setFavorite({
+    required String uid,
+    required String recipeId,
+    required bool isFavorite,
+  });
+}
